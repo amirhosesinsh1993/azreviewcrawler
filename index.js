2d1919dca984b2fd3c93093e78907b27a33c0669
amirhossein <s.amir.s1372@gmail.com>
2020-05-19 19:14:17 +0430

    Using concurrency and clustring

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-import puppeteer from 'puppeteer'
+const { Cluster } = require('puppeteer-cluster');
+const fs = require('fs');
 
-const crawler = async (url) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
-    await page.waitForSelector('.review-text-content')
+(async () => {
+    let db = []
 
-    // page.on('console', console.log)
-
-
-    const reviews = await page.evaluate(() => {
-        const revsDiv = document.body.querySelectorAll('.review-text-content')
-        const reviewInnerText = []
-        for(let i = 0; i <= 9; i++)
-            reviewInnerText.push(revsDiv[i].innerText)
-        return reviewInnerText
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+        monitor: true,
     })
 
-    console.log(reviews, reviews.length)
+    // Event handler to be called in case of problems
+    cluster.on('taskerror', (err, data) => {
+        console.log(`Error crawling ${data}: ${err.message}`)
+    })
 
+    await cluster.task(async ({ page, data: url }) => {
+        await page.goto(url)
+        await page.waitForSelector('.review-text-content')
 
-    await browser.close()
-}
+        const reviews = await page.evaluate(() => {
+            return [...document.body.querySelectorAll('.review-text-content')].map(e => e.innerText)
+        })
 
+        reviews.forEach(i => db.push(i))
+    })
 
-crawler('https://www.amazon.com/Apple-iPhone-Unlocked-Quad-Core-Smartphone/product-reviews/B01N4R20RS/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1')
+    const basedURL = 'https://www.amazon.com/Apple-iPhone-Totalmente-desbloqueado-Negro/product-reviews/B01N4R20RS/ref=cm_cr_arp_d_paging_btm_next_3?ie=UTF8&pageNumber='
 
-// review-text-content
-// https://www.amazon.com/Apple-iPhone-Unlocked-Quad-Core-Smartphone/product-reviews/B01N4R20RS/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1
-// https://www.amazon.com/Apple-iPhone-Unlocked-Quad-Core-Smartphone/product-reviews/B01N4R20RS/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=500
+    for (let index = 1; index <= 5; index++) {
+        cluster.queue(basedURL + index)
+    }
+
+    await cluster.idle()
+
+    // console.log(db)
+    console.log(db.length)
+
+    const text = db.map(JSON.stringify)
+        .join('\n')
+        .replace(/(^\[)|(\]$)/mg, '')
+
+    fs.writeFile('reviews.txt', text, 'utf8', function(err) {
+        if (err) {
+            console.log('Some error occured - file either not saved or corrupted file saved.');
+        } else {
+            console.log('It\'s saved!');
+        }
+    })
+
+    await cluster.close()
+})()
